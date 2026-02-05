1775f42192171b08e3bf15a1d4df2624019ba380
sabarop <sabar.pakpahan@gmail.com>
2026-02-05 22:55:11 +0700

    platina: gps: Move to Android.bp
    Change-Id: I62f04196be125f7595062020abab1976ab6ac70e

--- device.mk
+++ device.mk
@@ -173,6 +173,8 @@ PRODUCT_PACKAGES += \
     android.hardware.gnss@2.0.vendor \
     android.hardware.gnss@2.0-impl-qti \
     android.hardware.gnss@2.0-service-qti \
+    flp.conf \
+    gps.conf \
     libbatching \
     libgeofencing \
     libgnss \
@@ -180,8 +182,6 @@ PRODUCT_PACKAGES += \
     libwifi-hal-ctrl
 
 PRODUCT_COPY_FILES += \
-    $(DEVICE_PATH)/gps/etc/flp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/flp.conf \
-    $(DEVICE_PATH)/gps/etc/gps.conf:$(TARGET_COPY_OUT_VENDOR)/etc/gps.conf \
     $(DEVICE_PATH)/gps/etc/izat.conf:$(TARGET_COPY_OUT_VENDOR)/etc/izat.conf \
     $(DEVICE_PATH)/gps/etc/lowi.conf:$(TARGET_COPY_OUT_VENDOR)/etc/lowi.conf \
     $(DEVICE_PATH)/gps/etc/sap.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sap.conf \
