a79292178f9f9246f6d8863da38f2ac5590d5878
Peter Meiser <peter.meiser@gmx.com>
2026-02-07 13:16:21 +0100

    sdm660-common: Switch to crdroid doze
    Also drop PulseProximity and PulseTilt - it doesn't work
    
    Change-Id: Ic00db3485ac04bc808a9d222e8884f64f7d5958b

--- device.mk
+++ device.mk
@@ -433,7 +433,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.frameworks.sensorservice@1.0 \
     android.frameworks.sensorservice@1.0.vendor \
-    android.hardware.sensors@1.0-impl:64 \
+    android.hardware.sensors@1.0-impl-xiaomi \
     android.hardware.sensors@1.0-service \
     libpower.vendor
 
