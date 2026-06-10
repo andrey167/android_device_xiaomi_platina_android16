0f7e8e1a78d1869a466911b8e7059c6430ac8573
andrey167 <andrey_167@bk.ru>
2026-06-10 12:21:56 +0700

    platina: rootdir: Clean up post-boot and power initialization
    Remove redundant entries, group related settings, and
    improve readability without changing runtime behavior.
    
    Change-Id: I39a1d5ffbc1e74ae5a530a26d78879f3628bfa64

--- rootdir/bin/init.qcom.post_boot.sh
+++ rootdir/bin/init.qcom.post_boot.sh
@@ -27,128 +27,6 @@
 # ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
 
-function configure_read_ahead_kb_values() {
-    echo 512 > /sys/block/mmcblk0/bdi/read_ahead_kb
-    echo 512 > /sys/block/mmcblk0/queue/read_ahead_kb
-    echo 512 > /sys/block/mmcblk0rpmb/bdi/read_ahead_kb
-    echo 512 > /sys/block/mmcblk0rpmb/queue/read_ahead_kb
-    echo 512 > /sys/block/dm-0/queue/read_ahead_kb
-    echo 512 > /sys/block/dm-1/queue/read_ahead_kb
-    echo 512 > /sys/block/dm-2/queue/read_ahead_kb
-}
-
-function configure_memory_parameters() {
-    # Set Memory parameters.
-    #
-    # Set per_process_reclaim tuning parameters
-    # All targets will use vmpressure range 50-70,
-    # All targets will use 512 pages swap size.
-    #
-    # Set Low memory killer minfree parameters
-    # 64 bit will use Google default LMK series.
-    #
-    # Set ALMK parameters (usually above the highest minfree values)
-    # vmpressure_file_min threshold is always set slightly higher
-    # than LMK minfree's last bin value for all targets. It is calculated as
-    # vmpressure_file_min = (last bin - second last bin ) + last bin
-    #
-    # Set allocstall_threshold to 0 for all targets.
-    #
-
-    # Read adj series and set adj threshold for PPR and ALMK.
-    # This is required since adj values change from framework to framework.
-    adj_series=`cat /sys/module/lowmemorykiller/parameters/adj`
-    adj_1="${adj_series#*,}"
-    set_almk_ppr_adj="${adj_1%%,*}"
-
-    # PPR and ALMK should not act on HOME adj and below.
-    # Normalized ADJ for HOME is 6. Hence multiply by 6
-    # ADJ score represented as INT in LMK params, actual score can be in decimal
-    # Hence add 6 considering a worst case of 0.9 conversion to INT (0.9*6).
-    # For uLMK + Memcg, this will be set as 6 since adj is zero.
-    set_almk_ppr_adj=$(((set_almk_ppr_adj * 6) + 6))
-    echo $set_almk_ppr_adj > /sys/module/lowmemorykiller/parameters/adj_max_shift
-
-    # Calculate vmpressure_file_min as below & set:
-    # vmpressure_file_min = last_lmk_bin + (last_lmk_bin - last_but_one_lmk_bin)
-    minfree_series=`cat /sys/module/lowmemorykiller/parameters/minfree`
-    minfree_1="${minfree_series#*,}" ; rem_minfree_1="${minfree_1%%,*}"
-    minfree_2="${minfree_1#*,}" ; rem_minfree_2="${minfree_2%%,*}"
-    minfree_3="${minfree_2#*,}" ; rem_minfree_3="${minfree_3%%,*}"
-    minfree_4="${minfree_3#*,}" ; rem_minfree_4="${minfree_4%%,*}"
-    minfree_5="${minfree_4#*,}"
-
-    vmpres_file_min=$((minfree_5 + (minfree_5 - rem_minfree_4)))
-    echo $vmpres_file_min > /sys/module/lowmemorykiller/parameters/vmpressure_file_min
-    if [ $MemTotal -lt 4194304 ]; then
-        echo "18432,23040,27648,38708,120640,144768" > /sys/module/lowmemorykiller/parameters/minfree
-    elif [ $MemTotal -lt 6291456 ]; then
-        echo "18432,23040,27648,64512,165888,225792" > /sys/module/lowmemorykiller/parameters/minfree
-    else
-        echo "18432,23040,27648,96768,276480,362880" > /sys/module/lowmemorykiller/parameters/minfree
-    fi
-
-    # Enable adaptive LMK for all targets &
-    # use Google default LMK series for all 64-bit targets >=2GB.
-    echo 1 > /sys/module/lowmemorykiller/parameters/enable_adaptive_lmk
-
-    # Enable oom_reaper
-    if [ -f /sys/module/lowmemorykiller/parameters/oom_reaper ]; then
-        echo 1 > /sys/module/lowmemorykiller/parameters/oom_reaper
-    fi
-
-    # Set PPR parameters
-    echo $set_almk_ppr_adj > /sys/module/process_reclaim/parameters/min_score_adj
-    echo 0 > /sys/module/process_reclaim/parameters/enable_process_reclaim
-    echo 50 > /sys/module/process_reclaim/parameters/pressure_min
-    echo 70 > /sys/module/process_reclaim/parameters/pressure_max
-    echo 30 > /sys/module/process_reclaim/parameters/swap_opt_eff
-    echo 512 > /sys/module/process_reclaim/parameters/per_swap_size
-
-    # Set allocstall_threshold to 0 for all targets.
-    # Set swappiness to 100 for all targets
-    echo 0 > /sys/module/vmpressure/parameters/allocstall_threshold
-    echo 100 > /proc/sys/vm/swappiness
-
-    # Disable wsf for all targets beacause we are using efk.
-    # wsf Range : 1..1000 So set to bare minimum value 1.
-    echo 1 > /proc/sys/vm/watermark_scale_factor
-
-    configure_read_ahead_kb_values
-}
-
-# Set the default IRQ affinity to the primary cluster. When a
-# CPU is isolated/hotplugged, the IRQ affinity is adjusted
-# to one of the CPU from the default IRQ affinity mask.
-echo f > /proc/irq/default_smp_affinity
-
-if [ -f /sys/devices/soc0/soc_id ]; then
-        soc_id=`cat /sys/devices/soc0/soc_id`
-else
-        soc_id=`cat /sys/devices/system/soc/soc0/id`
-fi
-
-if [ -f /sys/devices/soc0/hw_platform ]; then
-        hw_platform=`cat /sys/devices/soc0/hw_platform`
-else
-        hw_platform=`cat /sys/devices/system/soc/soc0/hw_platform`
-fi
-
-panel=`cat /sys/class/graphics/fb0/modes`
-if [ "${panel:5:1}" == "x" ]; then
-    panel=${panel:2:3}
-else
-    panel=${panel:2:4}
-fi
-
-if [ $panel -gt 1080 ]; then
-    echo 2 > /proc/sys/kernel/sched_window_stats_policy
-    echo 5 > /proc/sys/kernel/sched_ravg_hist_size
-else
-    echo 3 > /proc/sys/kernel/sched_window_stats_policy
-    echo 3 > /proc/sys/kernel/sched_ravg_hist_size
-fi
-
 #Apply settings for sdm660, sdm636,sda636
 echo 2 > /sys/devices/system/cpu/cpu4/core_ctl/min_cpus
 echo 60 > /sys/devices/system/cpu/cpu4/core_ctl/busy_up_thres
@@ -171,60 +49,8 @@ echo 100000 > /proc/sys/kernel/sched_short_burst_ns
 echo 1 > /proc/sys/kernel/sched_prefer_sync_wakee_to_waker
 echo 20 > /proc/sys/kernel/sched_small_wakee_task_load
 
-# cpuset settings
-echo 0-3 > /dev/cpuset/background/cpus
-echo 0-3 > /dev/cpuset/system-background/cpus
-
 # disable thermal bcl hotplug to switch governor
 echo 0 > /sys/module/msm_thermal/core_control/enabled
-
-# online CPU0
-echo 1 > /sys/devices/system/cpu/cpu0/online
-
-# configure governor settings for little cluster
-echo "interactive" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
-echo 1 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/use_sched_load
-echo 1 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/use_migration_notif
-cho "19000 1401600:39000" > /sys/devices/system/cpu/cpu0/cpufreq/interactive/above_hispeed_delay
-echo 90 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/go_hispeed_load
-echo 20000 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/timer_rate
-echo 1401600 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/hispeed_freq
-echo 0 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/io_is_busy
-echo "85 1747200:95" > /sys/devices/system/cpu/cpu0/cpufreq/interactive/target_loads
-echo 39000 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/min_sample_time
-echo 0 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/max_freq_hysteresis
-echo 633600 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
-echo 1 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/ignore_hispeed_on_notif
-echo 1 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/fast_ramp_down
-
-# online CPU4
-echo 1 > /sys/devices/system/cpu/cpu4/online
-
-# configure governor settings for big cluster
-echo "interactive" > /sys/devices/system/cpu/cpu4/cpufreq/scaling_governor
-echo 1 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/use_sched_load
-echo 1 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/use_migration_notif
-echo "19000 1401600:39000" > /sys/devices/system/cpu/cpu4/cpufreq/interactive/above_hispeed_delay
-echo 90 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/go_hispeed_load
-echo 20000 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/timer_rate
-echo 1401600 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/hispeed_freq
-echo 0 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/io_is_busy
-echo "85 1401600:90 2150400:95" > /sys/devices/system/cpu/cpu4/cpufreq/interactive/target_loads
-echo 39000 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/min_sample_time
-echo 59000 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/max_freq_hysteresis
-echo 1113600 > /sys/devices/system/cpu/cpu4/cpufreq/scaling_min_freq
-echo 1 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/ignore_hispeed_on_notif
-echo 1 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/fast_ramp_down
-
-# bring all cores online
-echo 1 > /sys/devices/system/cpu/cpu0/online
-echo 1 > /sys/devices/system/cpu/cpu1/online
-echo 1 > /sys/devices/system/cpu/cpu2/online
-echo 1 > /sys/devices/system/cpu/cpu3/online
-echo 1 > /sys/devices/system/cpu/cpu4/online
-echo 1 > /sys/devices/system/cpu/cpu5/online
-echo 1 > /sys/devices/system/cpu/cpu6/online
-echo 1 > /sys/devices/system/cpu/cpu7/online
 
 # configure LPM
 echo N > /sys/module/lpm_levels/system/pwr/cpu0/ret/idle_enabled
@@ -245,9 +71,6 @@ echo 0 > /sys/module/lpm_levels/parameters/sleep_disabled
 
 # re-enable thermal and BCL hotplug
 echo 1 > /sys/module/msm_thermal/core_control/enabled
-
-# Set Memory parameters
-configure_memory_parameters
 
 # Enable bus-dcvs
 for cpubw in /sys/class/devfreq/*qcom,cpubw*
@@ -281,10 +104,6 @@ echo "cpufreq" > /sys/class/devfreq/soc:qcom,mincpubw/governor
 
 # Start cdsprpcd only for sdm660 and disable for sdm630 and sdm636
 start vendor.cdsprpcd
-
-chown -h system /sys/devices/system/cpu/cpufreq/ondemand/sampling_rate
-chown -h system /sys/devices/system/cpu/cpufreq/ondemand/sampling_down_factor
-chown -h system /sys/devices/system/cpu/cpufreq/ondemand/io_is_busy
 
 emmc_boot=`getprop vendor.boot.emmc`
 case "$emmc_boot"
