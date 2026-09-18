edc26915aba8821474681207cb29b401c10c4822
andrey167 <andrey_167@bk.ru>
2026-09-18 12:23:39 +0400

    Platina: Stay Derp

--- lineage_platina.mk
+++ lineage_platina.mk
@@ -38,3 +38,8 @@ TARGET_VENDOR_PRODUCT_NAME := platina
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="platina-user 10 QKQ1.190910.002 V12.0.3.0.QDTMIXM release-keys" \
     BuildFingerprint=Xiaomi/platina/platina:10/QKQ1.190910.002/V12.0.3.0.QDTMIXM:user/release-keys
+
+# Derpfest=AOSP Flags
+DERPFEST_BUILD_TYPE := UNOfficial
+DERPFEST_BUILD_VARIANT := Stable
+TARGET_ENABLE_BLUR := true
